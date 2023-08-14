138629f78ff1cae4413f798773a0865b70548a79
hoonloper <yonghoonj98@gmail.com>
2023-08-14 19:29:07 +0900

    Feat: PostService 생성

--- src/main/java/threads/server/application/controller/PostController.java
+++ src/main/java/threads/server/application/controller/PostController.java
@@ -2,8 +2,14 @@ package threads.server.application.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import threads.server.domain.post.PostService;
 
 @RestController
 @RequestMapping("/api/v1/post")
 public class PostController {
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
 }
---
+++ src/main/java/threads/server/domain/post/PostService.java
@@ -0,0 +1,7 @@
+package threads.server.domain.post;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+}
