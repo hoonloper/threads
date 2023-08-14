492dbd27fb5c658947d584f8bf42f28a413d0522
hoonloper <yonghoonj98@gmail.com>
2023-08-14 19:27:40 +0900

    Setting: 도메인별 컨트롤러 생성

---
+++ src/main/java/threads/server/application/controller/FollowController.java
@@ -0,0 +1,9 @@
+package threads.server.application.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/follow")
+public class FollowController {
+}
