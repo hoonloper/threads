dca2f8018a8c81198068cf9504ced1d6ad7946fd
hoonloper <yonghoonj98@gmail.com>
2023-08-14 20:43:41 +0900

    Setting: 댓글 API 구현을 위한 초기 세팅

--- src/main/java/threads/server/application/controller/CommentController.java
+++ src/main/java/threads/server/application/controller/CommentController.java
@@ -2,8 +2,14 @@ package threads.server.application.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import threads.server.domain.comment.CommentService;
 
 @RestController
 @RequestMapping("/api/v1/comment")
 public class CommentController {
+    private final CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
 }
---
+++ src/main/java/threads/server/domain/comment/CommentDTO.java
@@ -0,0 +1,13 @@
+package threads.server.domain.comment;
+
+import java.time.LocalDateTime;
+
+public record CommentDTO(
+        Long id,
+        Long userId,
+        Long postId,
+        String content,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
+) {
+}
