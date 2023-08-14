1f647ff0d54f36311c839ad8a256769a946ec651
hoonloper <yonghoonj98@gmail.com>
2023-08-14 19:38:14 +0900

    Feat: PostRepository 생성

--- src/main/java/threads/server/domain/post/PostService.java
+++ src/main/java/threads/server/domain/post/PostService.java
@@ -4,4 +4,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PostService {
+    private final PostRepository postRepository;
+
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
 }
