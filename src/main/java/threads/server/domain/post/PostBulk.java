979a760ce6a40e32d38656192d745b5156d2c4c2
hoonloper <yonghoonj98@gmail.com>
2023-08-17 20:34:20 +0900

    Feat: DB MySQL로 변경 및 쓰레드 배치 코드 및 테스트 구현

---
+++ src/main/java/threads/server/domain/post/PostBulk.java
@@ -0,0 +1,42 @@
+package threads.server.domain.post;
+
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
+@Repository
+public class PostBulk {
+    private final JdbcTemplate jdbcTemplate;
+
+    public PostBulk(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Transactional
+    public void bulkInsert(int batchSize) {
+        batchInsert(batchSize);
+    }
+
+    private void batchInsert(int batchSize) {
+        String sql = "INSERT INTO posts (user_id, content, created_at, last_modified_at) values ( ?, ?, ?, ? )";
+        jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                ps.setLong(1, 1);
+                ps.setString(2, "테스트" + (i + 1));
+                ps.setObject(3, LocalDateTime.now());
+                ps.setObject(4, LocalDateTime.now());
+            }
+
+            @Override
+            public int getBatchSize() {
+                return batchSize;
+            }
+        });
+    }
+}
---
+++ src/test/java/threads/server/domain/post/PostBulkTest.java
@@ -0,0 +1,17 @@
+package threads.server.domain.post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@Transactional // SQL이 롤백이 된다.
+@SpringBootTest
+class PostBulkTest {
+    @Autowired
+    private PostBulk postBulk;
+
+    @Test
+    void bulkInsert() {
+        postBulk.bulkInsert(100_000);
+    }
+}
