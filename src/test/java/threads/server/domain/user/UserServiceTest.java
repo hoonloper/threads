06296499b8d6e2ef55628961591b37586c59c120
hoonloper <yonghoonj98@gmail.com>
2023-08-22 21:34:28 +0900

    Test: 회원가입 존재하는 이메일 테스트

--- src/test/java/threads/server/domain/user/UserServiceTest.java
+++ src/test/java/threads/server/domain/user/UserServiceTest.java
@@ -7,10 +7,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import threads.server.application.exception.BadRequestException;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -46,9 +49,8 @@ public class UserServiceTest {
             User user = new User(1L, userDto.email(), userDto.password(), userDto.nickname(), userDto.name(), userDto.userRole());
             when(userRepository.save(any(User.class))).thenReturn(user);
 
-            UserService memberService = new UserService(userRepository);
-            System.out.println(userDto.toString());
-            UserDTO resultUserDto = memberService.signUp(userDto);
+            UserService userService = new UserService(userRepository);
+            UserDTO resultUserDto = userService.signUp(userDto);
 
             assertThat(resultUserDto).isNotNull();
             assertThat(resultUserDto.id()).isNotNull().isEqualTo(user.getId());
@@ -56,6 +58,23 @@ public class UserServiceTest {
             assertThat(resultUserDto.password()).isNull();
             assertThat(resultUserDto.name()).isEqualTo(user.getName());
             assertThat(resultUserDto.nickname()).isEqualTo(user.getNickname());
+        }
+    }
+
+    @Nested
+    @DisplayName("실패 케이스")
+    class Fail {
+        @Test
+        @DisplayName("회원가입 존재하는 이메일")
+        void signUp() {
+            String FAIL_MESSAGE = "이미 존재하는 이메일입니다.";
+            when(userRepository.findByEmail(any(String.class))).thenThrow(new BadRequestException(FAIL_MESSAGE));
+
+            UserService userService = new UserService(userRepository);
+            assertThatThrownBy(() -> userService.signUp(UserDTO.builder().email("").build()))
+                    .isExactlyInstanceOf(BadRequestException.class)
+                    .isInstanceOf(Exception.class)
+                    .hasMessage(FAIL_MESSAGE);
         }
     }
 }
