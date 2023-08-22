3d5794d19b91b0fd18ea51e49081fd9acafe91a7
hoonloper <yonghoonj98@gmail.com>
2023-08-22 20:57:26 +0900

    Feat: 초간단 로그인 기능 구현

--- src/main/java/threads/server/domain/user/UserService.java
+++ src/main/java/threads/server/domain/user/UserService.java
@@ -1,8 +1,12 @@
 package threads.server.domain.user;
 
+import com.sun.jdi.InternalException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import threads.server.application.exception.BadRequestException;
+import threads.server.application.exception.NotFoundException;
+
+import java.util.Optional;
 
 import static threads.server.domain.user.UserDTO.toDto;
 
@@ -16,7 +20,13 @@ public class UserService {
             throw new BadRequestException("이미 존재하는 이메일입니다.");
         });
 
-        User user = userRepository.save(new User(null, userDTO.email(), userDTO.name(), userDTO.nickname(), userDTO.userRole()));
+        User user = userRepository.save(new User(null, userDTO.email(), userDTO.password(), userDTO.name(), userDTO.nickname(), userDTO.userRole()));
+        return toDto(user);
+    }
+
+    public UserDTO signIn(UserDTO userDTO) {
+        String password = userDTO.password();
+        User user = userRepository.findOneByEmailAndPassword(userDTO.email(), password).orElseThrow(() -> new NotFoundException("회원 정보가 없습니다."));
         return toDto(user);
     }
 }
