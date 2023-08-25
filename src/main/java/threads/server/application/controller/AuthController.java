b95a06190d2237b9e24188501818d4ba92807ac6
hoonloper <yonghoonj98@gmail.com>
2023-08-25 20:49:57 +0900

    Feat: AuthController 스웨거 적용

--- src/main/java/threads/server/application/controller/AuthController.java
+++ src/main/java/threads/server/application/controller/AuthController.java
@@ -1,6 +1,11 @@
 package threads.server.application.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,14 +18,30 @@ import threads.server.domain.user.UserService;
 public class AuthController {
     private final UserService userService;
 
+    @Operation(summary = "회원 가입 요청", description = "회원 정보가 등록됩니다.", tags = { "인증 API" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "CREATED",
+                    content = @Content(schema = @Schema(implementation = UserDTO.class))
+            ),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+    })
     @PostMapping("sign-up")
     @ResponseStatus(HttpStatus.CREATED)
     public UserDTO signUp(@RequestBody UserDTO userDTO) {
         return userService.signUp(userDTO);
     }
 
+
+    @Operation(summary = "로그인 요청", description = "로그인합니다.", tags = { "인증 API" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "CREATED",
+                    content = @Content(schema = @Schema(implementation = UserDTO.class))
+            ),
+    })
     @PostMapping("sign-in")
-    public UserDTO getMyUserInfo(@RequestBody UserDTO userDTO) {
+    public UserDTO signIn(@RequestBody UserDTO userDTO) {
         return userService.signIn(userDTO);
     }
 }
