02cfa49abe315e62ade1e110dbddad3959ba4577
hoonloper <yonghoonj98@gmail.com>
2023-08-26 13:19:25 +0900

    Feat: 좋아요 API 스웨거 문서 작성

--- src/main/java/threads/server/application/controller/LikeController.java
+++ src/main/java/threads/server/application/controller/LikeController.java
@@ -1,6 +1,9 @@
 package threads.server.application.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +20,11 @@ public class LikeController {
     private final LikePostService likePostService;
     private final LikeCommentService likeCommentService;
 
+
+    @Operation(summary = "좋아요 하기", description = "쓰레드 or 댓글을 '좋아요'합니다.", tags = { "좋아요 API" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "CREATED"),
+    })
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void like(@RequestBody LikeDTO likeDTO) {
@@ -29,6 +37,10 @@ public class LikeController {
         throw new BadRequestException("잘못된 타입입니다.");
     }
 
+    @Operation(summary = "좋아요 취소", description = "쓰레드 or 댓글 좋아요를 취소합니다.", tags = { "좋아요 API" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "NO_CONTENT"),
+    })
     @DeleteMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void removeLike(@RequestBody LikeDTO likeDTO) {
