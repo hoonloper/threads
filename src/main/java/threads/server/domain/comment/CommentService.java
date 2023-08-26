b4ab564683ef76d185a0d9cb6f6000b7526b664e
hoonloper <yonghoonj98@gmail.com>
2023-08-26 23:06:31 +0900

    Feat: 댓글 수정 기능 예외 처리 추가

--- src/main/java/threads/server/domain/comment/CommentService.java
+++ src/main/java/threads/server/domain/comment/CommentService.java
@@ -3,6 +3,7 @@ package threads.server.domain.comment;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import threads.server.application.exception.NotFoundException;
+import threads.server.application.exception.UnauthorizedException;
 import threads.server.domain.comment.dto.CommentDTO;
 import threads.server.domain.comment.dto.CreatingCommentDTO;
 import threads.server.domain.comment.dto.UpdatingCommentDTO;
@@ -16,15 +17,16 @@ import static threads.server.domain.comment.dto.CommentDTO.toCommentDto;
 public class CommentService {
     private final CommentRepository commentRepository;
 
-    public CommentDTO save(CreatingCommentDTO commentDTO) {
-        User user = new User(commentDTO.userId());
-        Post post = new Post(commentDTO.postId());
-        return toCommentDto(commentRepository.save(new Comment(null, user, post, commentDTO.content())));
+    public CommentDTO save(CreatingCommentDTO commentDto) {
+        User user = new User(commentDto.userId());
+        Post post = new Post(commentDto.postId());
+        return toCommentDto(commentRepository.save(new Comment(null, user, post, commentDto.content())));
     }
 
-    public CommentDTO update(UpdatingCommentDTO commentDTO) {
-        Comment comment = commentRepository.findById(commentDTO.id()).orElseThrow(() -> new NotFoundException("댓글을 찾을 수 없습니다."));
-        comment.change(commentDTO.content());
+    public CommentDTO update(UpdatingCommentDTO commentDto) {
+        Comment comment = commentRepository.findById(commentDto.id()).orElseThrow(() -> new NotFoundException("댓글을 찾을 수 없습니다."));
+        authorizeUser(commentDto.userId(), comment.getUser().getId());
+        comment.change(commentDto.content());
         commentRepository.save(comment);
         return toCommentDto(comment);
     }
@@ -32,5 +34,12 @@ public class CommentService {
     public void delete(Long commentId) {
         Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new NotFoundException("댓글을 찾을 수 없습니다."));
         commentRepository.delete(comment);
+    }
+
+
+    private void authorizeUser(Long requestUserId, Long userIdFromComment) {
+        if (!requestUserId.equals(userIdFromComment)) {
+            throw new UnauthorizedException("권한이 없습니다.");
+        }
     }
 }
