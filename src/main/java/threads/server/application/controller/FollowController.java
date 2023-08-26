7c69ef052cfa364c9e4cbdb2b8657d3c61ef0124
hoonloper <yonghoonj98@gmail.com>
2023-08-26 13:04:39 +0900

    Feat: 팔로우 API 스웨거 문서 작성

--- src/main/java/threads/server/application/controller/FollowController.java
+++ src/main/java/threads/server/application/controller/FollowController.java
@@ -1,8 +1,14 @@
 package threads.server.application.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import threads.server.domain.comment.dto.CommentDTO;
 import threads.server.domain.follow.FollowDTO;
 import threads.server.domain.follow.FollowService;
 
@@ -12,12 +18,23 @@ import threads.server.domain.follow.FollowService;
 public class FollowController {
     private final FollowService followService;
 
+    @Operation(summary = "팔로우 하기", description = "유저를 팔로우합니다.", tags = { "팔로우 API" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "CREATED",
+                    content = @Content(schema = @Schema(implementation = FollowDTO.class))
+            ),
+    })
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void follow(@RequestBody FollowDTO followDTO) {
         followService.follow(followDTO);
     }
 
+
+    @Operation(summary = "팔로우 끊기", description = "유저 팔로우를 끊습니다.", tags = { "팔로우 API" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "NO_CONTENT"),
+    })
     @DeleteMapping("{followId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void unfollow(@PathVariable("followId") Long followId) {
