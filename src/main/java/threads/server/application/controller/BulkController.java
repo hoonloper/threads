3d0d8be85cd726bfbdd5e8d5b8cc2650188f1367
hoonloper <yonghoonj98@gmail.com>
2023-08-28 20:08:08 +0900

    Feat: 벌크 API 통합

--- src/main/java/threads/server/application/controller/BulkController.java
+++ src/main/java/threads/server/application/controller/BulkController.java
@@ -4,41 +4,25 @@ package threads.server.application.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import threads.server.domain.bulk.CommentBulk;
-import threads.server.domain.bulk.FollowBulk;
-import threads.server.domain.bulk.PostBulk;
-import threads.server.domain.bulk.UserBulk;
+import threads.server.domain.bulk.*;
 
 @RestController
-@RequestMapping("/api/v1/bulk")
+@RequestMapping("/api/bulk")
 @RequiredArgsConstructor
 public class BulkController {
     private final PostBulk postBulk;
     private final UserBulk userBulk;
     private final CommentBulk commentBulk;
     private final FollowBulk followBulk;
+    private final LikeBulk likeBulk;
 
-    @PostMapping("/user/{size}")
+    @PostMapping("/{size}")
     @ResponseStatus(HttpStatus.CREATED)
     public void bulkUser(@PathVariable("size") int size) {
         userBulk.bulkInsert(size);
-    }
-
-    @PostMapping("/post/{size}")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void bulkPost(@PathVariable("size") int size) {
         postBulk.bulkInsert(size);
-    }
-
-    @PostMapping("/comment/{size}")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void bulkComment(@PathVariable("size") int size) {
         commentBulk.bulkInsert(size);
-    }
-
-    @PostMapping("/follow/{size}")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void bulkFollow(@PathVariable("size") int size) {
-        followBulk.bulkInsert(size);
+        followBulk.bulkInsert(size - 5);
+        likeBulk.bulkInsert(size - 5);
     }
 }
