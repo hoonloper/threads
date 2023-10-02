7ec98d62ceaea47ac109a8fc5ca88597a5ceb8f0
hoonloper <yonghoonj98@gmail.com>
2023-10-02 12:47:11 +0900

    Feat: 유저 팔로우, 언팔로우 검증 코드 추가

--- src/main/java/threads/server/domain/follow/FollowService.java
+++ src/main/java/threads/server/domain/follow/FollowService.java
@@ -2,6 +2,7 @@ package threads.server.domain.follow;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import threads.server.application.exception.BadRequestException;
 import threads.server.application.exception.NotFoundException;
 import threads.server.application.exception.UnauthorizedException;
 import threads.server.domain.follow.dto.FollowingDTO;
@@ -16,17 +17,22 @@ public class FollowService {
     private final FollowRepository followRepository;
 
     public void follow(FollowingDTO followDto) {
+        boolean followed = followRepository.findByToUserIdAndFromUserId(followDto.toUserId(), followDto.fromUserId()).isPresent();
+        if(Boolean.TRUE.equals(followed)) {
+            throw new BadRequestException("이미 팔로우 중 입니다.");
+        }
         User toUser = new User(followDto.toUserId());
         User fromUser = new User(followDto.fromUserId());
         followRepository.save(new Follow(null, toUser, fromUser, LocalDateTime.now()));
     }
 
     public void unfollow(UnfollowingDTO followDto) {
-        Follow follow = followRepository.findById(followDto.id()).orElseThrow(() -> new NotFoundException("팔로우 내역을 찾을 수 없습니다."));
+        Follow follow = followRepository.findByToUserIdAndFromUserId(followDto.toUserId(), followDto.fromUserId()).orElseThrow(() -> new NotFoundException("팔로우 내역을 찾을 수 없습니다."));
         if(!followDto.fromUserId().equals(follow.getFromUser().getId()) ||
             !followDto.toUserId().equals(follow.getToUser().getId())) {
             throw new UnauthorizedException("권한이 없습니다.");
         }
         followRepository.delete(follow);
     }
+
 }
