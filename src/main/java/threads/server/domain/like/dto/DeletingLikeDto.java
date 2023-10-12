5a24abdad1c0a65546cac890710af113f3d19ae0
hoonloper <yonghoonj98@gmail.com>
2023-10-12 20:03:12 +0900

    Refactor: LikeDto 리팩토링

--- src/main/java/threads/server/domain/like/dto/DeletingLikeDto.java
+++ src/main/java/threads/server/domain/like/dto/DeletingLikeDto.java
@@ -1,6 +1,5 @@
 package threads.server.domain.like.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Data;
