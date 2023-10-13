2c88637cbf7e9c12050a3741b022962cc878c745
hoonloper <yonghoonj98@gmail.com>
2023-10-13 18:42:03 +0900

    Feat: UserRole 개선

--- src/main/java/threads/server/domain/user/UserRole.java
+++ src/main/java/threads/server/domain/user/UserRole.java
@@ -1,5 +1,15 @@
 package threads.server.domain.user;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum UserRole {
-    ADMIN, USER
+
+    ADMIN("ROLE_ADMON", "관리자"),
+    USER("ROLE_USER", "일반 사용자");
+
+    private final String key;
+    private final String title;
 }
