e0951d47f35ee4100fec0a20463af7b7d5e928f6
hoonloper <yonghoonj98@gmail.com>
2023-10-14 23:59:36 +0900

    Feat: 쿼리 최적화로 사용하지 않게 된 쿼리 제거

--- src/main/java/threads/server/domain/like/repository/LikePostRepository.java
+++ src/main/java/threads/server/domain/like/repository/LikePostRepository.java
@@ -6,7 +6,5 @@ import threads.server.domain.like.entity.LikePost;
 import java.util.Optional;
 
 public interface LikePostRepository extends JpaRepository<LikePost, Long>, LikeRepository<LikePost> {
-    Long countByPostId(Long postId);
-
     Optional<LikePost> findByUserIdAndPostId(Long userId, Long postId);
 }
